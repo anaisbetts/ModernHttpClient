8127e26eb769dd78bc8bdafafc4b356f251e50ac
Nick Berardi <nberardi@gmail.com>
2013-10-07 15:39:06 -0400

    Updated to correct some "bad URL" errors that were being thrown
    "bad URL" were being thrown when a space appeared in any part of the URL. [NSUrl URLWithString:] properly encodes the URI.

--- src/ModernHttpClient.iOS/AFNetworkHandler.cs
+++ src/ModernHttpClient.iOS/AFNetworkHandler.cs
@@ -32,7 +32,7 @@ namespace ModernHttpClient
                     return acc;
                 }),
                 HttpMethod = request.Method.ToString().ToUpperInvariant(),
-                Url = new NSUrl(request.RequestUri.ToString()),
+                Url = NSUrl.FromString(request.RequestUri.AbsoluteUri),
             };
 
             var host = request.RequestUri.GetLeftPart(UriPartial.Authority);
