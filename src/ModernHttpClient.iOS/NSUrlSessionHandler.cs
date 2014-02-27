52c67eb7b5a5e3a5802c82392417dfb70cdf82cb
Paul Betts <paul@paulbetts.org>
2014-02-26 17:57:03 -0800

    Implement a blocking stream based on byte array segments. There's no way in hell this code is correct.

--- src/ModernHttpClient.iOS/NSUrlSessionHandler.cs
+++ src/ModernHttpClient.iOS/NSUrlSessionHandler.cs
@@ -13,8 +13,8 @@ namespace ModernHttpClient
     {
         readonly NSUrlSession session;
 
-        readonly Dictionary<NSUrlSessionTask, Tuple<HttpResponseMessage, CancellationToken>> inflightRequests = 
-            new Dictionary<NSUrlSessionTask, Tuple<HttpResponseMessage, CancellationToken>>();
+        readonly Dictionary<NSUrlSessionTask, Tuple<HttpResponseMessage, ByteArrayListStream, CancellationToken>> inflightRequests = 
+            new Dictionary<NSUrlSessionTask, Tuple<HttpResponseMessage, ByteArrayListStream, CancellationToken>>();
 
         public NSUrlSessionHandler()
         {
@@ -60,7 +60,7 @@ namespace ModernHttpClient
 
         class DataTaskDelegate : NSUrlSessionDataDelegate
         {
-            NSUrlSessionHandler This { get; protected set; }
+            NSUrlSessionHandler This { get; set; }
             public DataTaskDelegate(NSUrlSessionHandler that)
             {
                 this.This = that;
@@ -91,5 +91,142 @@ namespace ModernHttpClient
                 }
             }
         }
+
+        class ByteArrayListStream : Stream
+        {
+            IDisposable lockRelease = EmptyDisposable.Instance;
+            readonly AsyncLock readStreamLock = new AsyncLock();
+            readonly List<byte[]> bytes = new List<byte[]>();
+
+            bool isCompleted;
+            long maxLength = 0;
+            long position = 0;
+
+            public ByteArrayListStream()
+            {
+                // Initially we have nothing to read so Reads should be parked
+                readStreamLock.LockAsync().ContinueWith(t => lockRelease = t.Result);
+            }
+
+            public override bool CanRead { get { return true; } }
+            public override bool CanWrite { get { return false; } }
+            public override void Write(byte[] buffer, int offset, int count) { throw new NotSupportedException(); }
+            public override void WriteByte(byte value) { throw new NotSupportedException(); }
+            public override bool CanSeek { get { return true; } }
+            public override bool CanTimeout { get { return false; } }
+            public override void SetLength(long value) { throw new NotSupportedException(); }
+            public override void Flush() { }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            { 
+                var result = default(long);
+                switch (origin) {
+                case SeekOrigin.Begin:
+                    result = offset;
+                    break;
+                case SeekOrigin.Current:
+                    result = position + offset;
+                    break;
+                case SeekOrigin.End:
+                    result = maxLength + offset;
+                    break;
+                }
+                    
+                return (Position = result);
+            }
+
+            public override long Position {
+                get { return position; }
+                set {
+                    if (value < 0 || value > maxLength) {
+                        throw new ArgumentException();
+                    }
+
+                    // Seeking during a read? No way.
+                    lock (bytes) {
+                        position = value;
+                    }
+                }
+            }
+
+            public override long Length {
+                get {
+                    return maxLength;
+                }
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                return this.ReadAsync(buffer, offset, count).Result;
+            }
+
+            public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+            {
+                int bytesRead = 0;
+
+                using (await readStreamLock.LockAsync()) {
+                    lock (bytes) {
+                        int absPositionOfCurrentBuffer = 0;
+                        int destOffset = offset;
+
+                        foreach (var buf in bytes) {
+                            cancellationToken.ThrowIfCancellationRequested();
+
+                            // Get ourselves to the right buffer
+                            if (position > absPositionOfCurrentBuffer) {
+                                absPositionOfCurrentBuffer += buf.Length;
+                                continue;
+                            }
+
+                            int offsetInSrcBuffer = (int)position - absPositionOfCurrentBuffer;
+                            int toCopy = Math.Min(count, buf.Length - offsetInSrcBuffer);
+                            Array.ConstrainedCopy(buf, offsetInSrcBuffer, buffer, offset, toCopy);
+
+                            bytesRead += toCopy;
+                            offset += toCopy;
+                            position += toCopy;
+                            absPositionOfCurrentBuffer += toCopy;
+                            count -= toCopy;
+
+                            if (count < 0) break;
+                        }
+                    }
+                }
+
+                // If we're at the end of the stream and it's not done, prepare
+                // the next read to park itself unless AddByteArray or Complete 
+                // posts
+                if (position >= maxLength && !isCompleted) {
+                    readStreamLock.LockAsync().ContinueWith(t => lockRelease = t.Result);
+                }
+
+                return bytesRead;
+            }
+
+            public void AddByteArray(byte[] arrayToAdd)
+            {
+                lock (bytes) {
+                    maxLength += arrayToAdd.Length;
+                    bytes.Add(arrayToAdd);
+                }
+
+                Interlocked.Exchange(ref lockRelease, EmptyDisposable.Instance).Dispose();
+            }
+
+            public void Complete()
+            {
+                isCompleted = true;
+                Interlocked.Exchange(ref lockRelease, EmptyDisposable.Instance).Dispose();
+            }
+        }
+    }
+
+    sealed class EmptyDisposable : IDisposable
+    {
+        static readonly IDisposable instance = new EmptyDisposable();
+        public static IDisposable Instance { get { return instance; } }
+
+        EmptyDisposable() { }
+        public void Dispose() { }
     }
 }
