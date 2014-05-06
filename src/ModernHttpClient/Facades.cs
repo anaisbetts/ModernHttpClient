a892aefcf5028fe1ce418fffc5a8f7969959b94f
Paul Betts <paul@paulbetts.org>
2014-05-06 16:29:38 -0700

    Add a file with dummy classes

---
+++ src/ModernHttpClient/Facades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ModernHttpClient.Portable
+{
+    public class NativeMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            throw new Exception("You're referencing the Portable version in your App - you need to reference the platform (iOS/Android) version");
+        }
+    }
+}
+
