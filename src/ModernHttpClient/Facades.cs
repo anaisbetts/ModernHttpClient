3635e319b29f19cd9c1a3b789bbeef4e0a426318
Nick Berardi <nberardi@gmail.com>
2014-05-14 11:23:44 -0400

    added facade interfaces

--- src/ModernHttpClient/Facades.cs
+++ src/ModernHttpClient/Facades.cs
@@ -7,9 +7,18 @@ namespace ModernHttpClient.Portable
 {
     public class NativeMessageHandler : HttpMessageHandler
     {
+        public void RegisterForProgress(HttpRequestMessage request, ProgressDelegate callback)
+        {
+        }
+
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             throw new Exception("You're referencing the Portable version in your App - you need to reference the platform (iOS/Android) version");
         }
     }
+
+    public class ProgressStreamContent : StreamContent {
+    }
+
+    public delegate void ProgressDelegate (long bytes, long totalBytes, long totalBytesExpected);
 }
