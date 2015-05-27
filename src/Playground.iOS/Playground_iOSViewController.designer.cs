fc1c7ce74681e09cd8a5da9fe7d5e32102b506a9
David Potter <dpotter@tableau.com>
2015-05-26 18:07:49 -0700

    Add blank line at end of files and replace tabs with spaces to mollify source control triggers.

--- src/Playground.iOS/Playground_iOSViewController.designer.cs
+++ src/Playground.iOS/Playground_iOSViewController.designer.cs
@@ -9,35 +9,35 @@ using System.CodeDom.Compiler;
 
 namespace Playground.iOS
 {
-	[Register ("Playground_iOSViewController")]
-	partial class Playground_iOSViewController
-	{
+    [Register ("Playground_iOSViewController")]
+    partial class Playground_iOSViewController
+    {
         [Outlet]
         UIKit.UIProgressView progress { get; set; }
 
-		[Outlet]
-		UIKit.UILabel md5sum { get; set; }
+        [Outlet]
+        UIKit.UILabel md5sum { get; set; }
 
-		[Outlet]
-		UIKit.UITextView result { get; set; }
+        [Outlet]
+        UIKit.UITextView result { get; set; }
 
-		[Action ("cancelIt:")]
-		partial void cancelIt (Foundation.NSObject sender);
+        [Action ("cancelIt:")]
+        partial void cancelIt (Foundation.NSObject sender);
 
-		[Action ("doIt:")]
-		partial void doIt (Foundation.NSObject sender);
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (result != null) {
-				result.Dispose ();
-				result = null;
-			}
+        [Action ("doIt:")]
+        partial void doIt (Foundation.NSObject sender);
+        
+        void ReleaseDesignerOutlets ()
+        {
+            if (result != null) {
+                result.Dispose ();
+                result = null;
+            }
 
-			if (md5sum != null) {
-				md5sum.Dispose ();
-				md5sum = null;
-			}
-		}
-	}
+            if (md5sum != null) {
+                md5sum.Dispose ();
+                md5sum = null;
+            }
+        }
+    }
 }
